ce377ff87549e8008bdc8aa5a9a4eef4d2347121
mfurg <mfurgalo@gmail.com>
2022-07-26 14:13:04 +0300

    seed update

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.create({"email": "mfurgalo@gmail.com","password": "123456","first_name": "Maksym","last_name": "Furgalo","role": "admin"},
+            {"email": "oleg@mail.me","password": "123456","first_name": "Oleg","last_name": "Skosariev","role": "user"})
+
+Item.create({"name": "apple", "description": "Regular apple", "price": "10"})
+
