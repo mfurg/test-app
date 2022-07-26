8ec90e2c4e3d9cbc9368e343e11098909c1d8b70
mfurg <mfurgalo@gmail.com>
2022-07-26 14:37:55 +0300

    user create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,1 @@
-User.create({"email": "mfurgalo@gmail.com","password": "123456","first_name": "Maksym","last_name": "Furgalo","role": "admin"},
-            {"email": "oleg@mail.me","password": "123456","first_name": "Oleg","last_name": "Skosariev","role": "user"})
-
-Item.create({"name": "apple", "description": "Regular apple", "price": "10"})
-
+User.create({"email": "mfurgalo@gmail.com","password": "123456","first_name": "Maksym","last_name": "Furgalo","role": "admin"})
